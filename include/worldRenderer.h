5df41246182f07d14d16810f7d18bdcdc376e3d8
Lufu251 <luki.furrer@hotmail.ch>
2022-07-22 17:52:00 +0200

    fixed worldRenderer int overflow bug

--- include/worldRenderer.h
+++ include/worldRenderer.h
@@ -17,15 +17,18 @@ public:
         //calculate visible area
         int xSize = pWindow.getSize().x / pWorld.getTileSize() + 4;
         int ySize = pWindow.getSize().y / pWorld.getTileSize() + 4;
-        int xStartPosition = (1 + pCamera.getOffset().x() - pWindow.getSize().x /2) / pWorld.getTileSize() -2;
-        int yStartPosition = (1 + pCamera.getOffset().y() - pWindow.getSize().y/2) / pWorld.getTileSize() -2;
+
+        int xStartPosition = static_cast<int>((1 + pCamera.getOffset().x() - static_cast<int>(pWindow.getSize().x) / 2.f) / pWorld.getTileSize() -2);
+        int yStartPosition = static_cast<int>((1 + pCamera.getOffset().y() - static_cast<int>(pWindow.getSize().y) / 2.f) / pWorld.getTileSize() -2);
+        
         int xEndPosition = xStartPosition + xSize;
         int yEndPosition = yStartPosition + ySize;
+
         if(xStartPosition < 0){
             xStartPosition = 0;
         }
         if(yStartPosition < 0){
-            yStartPosition = 0;
+            yStartPosition = 1;
         }
         if(xEndPosition > pWorld.sizeX()){
             xEndPosition = pWorld.sizeX();
@@ -33,49 +36,49 @@ public:
         if(yEndPosition > pWorld.sizeY()){
             yEndPosition = pWorld.sizeY();
         }
-
+        std::cout << "xS: " << xStartPosition << " yS: " << yStartPosition << " xE: " << xEndPosition << " yE: " << yEndPosition << "\n";
         // loop visible area
         for (int x = xStartPosition; x < xEndPosition; x++){
             for (int y = yStartPosition; y < yEndPosition; y++){
-
                 int textureSize = 16;
                 float scaleX = static_cast<float>(pWorld.getTileSize()) / textureSize;
                 float scaleY = static_cast<float>(pWorld.getTileSize()) / textureSize;
 
-                switch (pWorld(x,y).getType())
-                {
-                    case Grass:
-                        sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
-                        sprite.setColor(sf::Color(255,255,255,255));
-                        sprite.setScale(scaleX, scaleY);
-                        sprite.setTexture(pTextures[grassT]);
-                    break;
-                    case Rock:
-                        sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
-                        sprite.setColor(sf::Color(255,255,255,255));
-                        sprite.setScale(scaleX, scaleY);
-                        sprite.setTexture(pTextures[rockT]);
-                    break;
-                    case Borium:
-                        sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
-                        sprite.setColor(sf::Color(255,255,255,255));
-                        sprite.setScale(scaleX, scaleY);
-                        sprite.setTexture(pTextures[boriumT]);
-                    break;
-                    case Beacore:
-                        sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
-                        sprite.setColor(sf::Color(255,255,255,255));
-                        sprite.setScale(scaleX, scaleY);
-                        sprite.setTexture(pTextures[beacoreT]);
-                    break;
-                    case Spawner:
-                        sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
-                        sprite.setColor(sf::Color(255,255,255,255));
-                        sprite.setScale(scaleX, scaleY);
-                        sprite.setTexture(pTextures[spawnerT]);
-                    break;
-                }
-            pWindow.draw(sprite);
+                    switch (pWorld(x,y).getType())
+                    {
+                        case Grass:
+                            sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
+                            
+                            sprite.setColor(sf::Color(255,255,255,255));
+                            sprite.setScale(scaleX, scaleY);
+                            sprite.setTexture(pTextures[grassT]);
+                        break;
+                        case Rock:
+                            sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
+                            sprite.setColor(sf::Color(255,255,255,255));
+                            sprite.setScale(scaleX, scaleY);
+                            sprite.setTexture(pTextures[rockT]);
+                        break;
+                        case Borium:
+                            sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
+                            sprite.setColor(sf::Color(255,255,255,255));
+                            sprite.setScale(scaleX, scaleY);
+                            sprite.setTexture(pTextures[boriumT]);
+                        break;
+                        case Beacore:
+                            sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
+                            sprite.setColor(sf::Color(255,255,255,255));
+                            sprite.setScale(scaleX, scaleY);
+                            sprite.setTexture(pTextures[beacoreT]);
+                        break;
+                        case Spawner:
+                            sprite.setPosition(static_cast<float>(x) * pWorld.getTileSize(), static_cast<float>(y) * pWorld.getTileSize());
+                            sprite.setColor(sf::Color(255,255,255,255));
+                            sprite.setScale(scaleX, scaleY);
+                            sprite.setTexture(pTextures[spawnerT]);
+                        break;
+                    }
+                pWindow.draw(sprite);
             }
         }
     }
